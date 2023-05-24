5876af9fefedae4c602e9367e5df92ad63c380f7
Mahmoud Osman <mahmoudosman.som@gmail.com>
2023-05-23 21:29:47 -0500

    added getAllEmployees @Test method

--- minsura/src/test/java/com/minsura/repository/EmployeeRepositoryTest.java
+++ minsura/src/test/java/com/minsura/repository/EmployeeRepositoryTest.java
@@ -2,12 +2,15 @@ package com.minsura.repository;
 
 
 import com.minsura.model.Employee;
-import org.assertj.core.api.Assert;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
 // Testing the EmployeeRepository
@@ -19,7 +22,7 @@ class EmployeeRepositoryTest {
 
 
     // JUnit test for save employee method
-    @DisplayName("JUnit test for save employee method")
+    @DisplayName("JUnit test for save employee")
     @Test
     public void given_EmployeeObject_whenSave_thenReturnSavedEmployeeObject() {
 
@@ -27,7 +30,7 @@ class EmployeeRepositoryTest {
 
         Employee employee = Employee.builder()
                 .firstName("Mahmoud")
-                .lastName("osman")
+                .lastName("Osman")
                 .email("osman778@yahoo.com")
                 .build();
 
@@ -42,5 +45,41 @@ class EmployeeRepositoryTest {
 
 
     }
+
+
+    // JUnit test for getAll employees method
+
+    @DisplayName("JUnit test for getAll employees")
+    @Test
+    public void givenEmployeesList_whenFindAll_thenReturnEmployeesList() {
+
+        //given - precondition or set up
+        //create employee object
+        Employee employee1 = Employee.builder()
+                .firstName("Mahmoud")
+                .lastName("osman")
+                .email("osman778@yahoo.com")
+                .build();
+
+        Employee employee2 = Employee.builder()
+                .firstName("John")
+                .lastName("Doe")
+                .email("john_doe900@yahoo.com")
+                .build();
+        //save the 2 employee objects
+        employeeRepository.save(employee1);
+        employeeRepository.save(employee2);
+
+        //when - action or the behavior that is being tested
+        //--findAll() method - the behavior that is being tested
+        List<Employee> employeeList = employeeRepository.findAll();
+
+
+        //then- verify the output
+        assertThat(employeeList).isNotNull();
+        assertThat(employeeList.size()).isEqualTo(2); // should return 2 object/employees
+
+    }
+
 
 }
